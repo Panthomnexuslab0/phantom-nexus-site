18b4bea94be731f230364d5203bf5c3d54523691
Phantom Nexus Labs <digitalprosper590@gmail.com>
2026-02-23 18:12:31 +0530

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-import SecurityArchitecture from "./components/SecurityArchitecture";
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
 import Terms from './pages/Terms';
